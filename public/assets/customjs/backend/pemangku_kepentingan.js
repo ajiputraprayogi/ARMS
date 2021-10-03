ce4975ce62d97f0cf2bce4e675996c19d357942f
ajiputraprayogi <ajiputraprayogi@gmail.com>
2021-10-03 16:58:07 +0700

    aji

--- public/assets/customjs/backend/pemangku_kepentingan.js
+++ public/assets/customjs/backend/pemangku_kepentingan.js
@@ -70,3 +70,44 @@ function hapusdatapemangku(kode){
         }
     })
 }
+function hapusdatapemangkupelaksanaan(kode){
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass:{
+            confirmButton: 'btn btn-success',
+            cancelButton:'btn btn-danger',
+        },
+        buttonsStyling: true
+    })
+    swalWithBootstrapButtons.fire({
+        title: 'Hapus Data ?',
+        text: "Data tidak dapat dipulihkan kembali!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Ya, Hapus!',
+        cancelButtonText: 'Tidak',
+        reverseButtons: true
+    }).then((result)=>{
+        if(result.value){
+            $.ajaxSetup({
+                headers:{
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            })
+            $.ajax({
+                type: 'DELETE',
+                url: '/pemangkukepentingan/'+kode,
+                data:{
+                    'token':$('input[name=_token]').val(),
+                },
+                success: function(){
+                    swalWithBootstrapButtons.fire(
+                        'Deleted!',
+                        'Data Berhasil Dihapus.',
+                        'success'
+                    )
+                    location.reload();
+                }
+            })
+        }
+    })
+}
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,13 @@ Route::group(['middleware' => ['auth','checkRole:Superadmin']],function(){
     });
     // Basis Data
     Route::resource('/pelaksanaan', 'backend\ManajemenresikoController');
+    Route::get('/edit-pelaksanaan/{id}', 'backend\ManajemenresikoController@edit');
+    Route::post('/simpan-edit-pelaksanaan/{id}', 'backend\ManajemenresikoController@update');
+    Route::get('/hapus-pelaksanaan/{id}', 'backend\ManajemenresikoController@destroy');
+    Route::post('simpan-edit-pemangku-pelaksanaan/{id}', 'backend\ManajemenresikoController@simpaneditpemangku');
+    Route::post('simpan-edit-edit-pemangku-pelaksanaan/{id}', 'backend\ManajemenresikoController@simpanediteditpemangku');
+    Route::post('simpan-edit-konteks-pelaksanaan/{id}', 'backend\ManajemenresikoController@simpaneditkonteks');
+    Route::post('simpan-edit-edit-konteks-pelaksanaan/{id}', 'backend\ManajemenresikoController@simpanediteditkonteks');
 
     Route::resource('konteks', 'backend\KonteksController');
     Route::get('data-konteks', 'backend\KonteksController@listdata');
