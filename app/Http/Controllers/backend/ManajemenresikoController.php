f00597a0c61e0e4fd306ccbf05f222756b99759a
ajiputraprayogi <ajiputraprayogi@gmail.com>
2021-10-03 13:29:53 +0700

    aji

--- app/Http/Controllers/backend/ManajemenresikoController.php
+++ app/Http/Controllers/backend/ManajemenresikoController.php
@@ -20,15 +20,14 @@ class ManajemenresikoController extends Controller
      */
     public function index()
     {
-        // $konteks = konteks::leftJoin('jenis_konteks','konteks.id_konteks','=','jenis_konteks.id')
-        // ->select('jenis_konteks.id as idjk','jenis_konteks.*','konteks.*')->where('faktur_konteks',$finalkode)->get();
-        // SELECT *, COUNT( * ) AS total FROM comment GROUP BY post_id
-        $pelaksanaanmanajemenrisiko = pelaksanaanmanajemenrisiko::leftJoin('departemen','pelaksanaan_manajemen_risiko.id_departemen','=','departemen.id')
-        // ->leftJoin('konteks','pelaksanaan_manajemen_risiko.faktur','=','konteks.faktur_konteks')
-        // ->leftJoin('pemangku_kepentingan','pelaksanaan_manajemen_risiko.faktur','=','pemangku_kepentingan.faktur_pemangku')
-        ->select('departemen.id as idd','departemen.nama as nama_departemen','pelaksanaan_manajemen_risiko.*')->get();
-        // $pelaksanaanmanajemenrisiko = pelaksanaanmanajemenrisiko::all();
-        return view('backend.manajemen_risiko.pelaksanaan_risiko',['pelaksanaanmanajemenrisiko'=>$pelaksanaanmanajemenrisiko]);
+        $data = DB::table('pelaksanaan_manajemen_risiko')
+        ->select(DB::raw('pelaksanaan_manajemen_risiko.*,count(*) as totalkonteks,departemen.nama'))
+        ->leftjoin('departemen','departemen.id','=','pelaksanaan_manajemen_risiko.id_departemen')
+        ->leftjoin('konteks','konteks.faktur_konteks','=','pelaksanaan_manajemen_risiko.faktur')
+        ->orderby('pelaksanaan_manajemen_risiko.id','desc')
+        ->groupby('pelaksanaan_manajemen_risiko.faktur')
+        ->get();
+        return view('backend.manajemen_risiko.pelaksanaan_risiko',compact('data'));
     }
 
     /**
@@ -89,17 +88,8 @@ class ManajemenresikoController extends Controller
             'selera_risiko'=>$request->selera_risiko,
         ]);
         return redirect('/pelaksanaan')->with('status','Sukses menyimpan data');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+  //
+ //
     }
 
     /**
